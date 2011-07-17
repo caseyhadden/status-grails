18322a0426391481a32ae92683a263b2a4f1e76e
Casey Hadden <me@caseyhadden.com>
2011-07-16 21:07:46 -0400

    Add threads information controller

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -101,6 +101,7 @@ grails.plugins.dynamicController.mixins = [
 	'com.burtbeckwith.grails.plugins.appinfo.SpringControllerMixin' :     'com.burtbeckwith.appinfo_test.AdminManageController',
 	'com.burtbeckwith.grails.plugins.appinfo.MemoryControllerMixin' :     'com.burtbeckwith.appinfo_test.AdminManageController',
 	'com.burtbeckwith.grails.plugins.appinfo.PropertiesControllerMixin' : 'com.burtbeckwith.appinfo_test.AdminManageController',
-	'com.burtbeckwith.grails.plugins.appinfo.ScopesControllerMixin' :     'com.burtbeckwith.appinfo_test.AdminManageController'
+	'com.burtbeckwith.grails.plugins.appinfo.ScopesControllerMixin' :     'com.burtbeckwith.appinfo_test.AdminManageController',
+	'com.burtbeckwith.grails.plugins.appinfo.ThreadsControllerMixin' :     'com.burtbeckwith.appinfo_test.AdminManageController'
 ]
 grails.plugins.appinfo.useContextListener = true
